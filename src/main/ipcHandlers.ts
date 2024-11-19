dba9676e4cb39465a9322c47e5f25a48d7d8efd3
therealrinku <rinkunited2032@gmail.com>
2024-11-19 17:25:42 +0545

    feat: wrap fetch call in catch and throw 404 for errors

--- src/main/ipcHandlers.ts
+++ src/main/ipcHandlers.ts
@@ -20,28 +20,41 @@ export function registerIpcHandlers(mainWindow: Electron.BrowserWindow | null) {
       method: args.reqType,
     };
 
-    const resp = await fetch(reqUrl, reqObj);
+    try {
+      const resp = await fetch(reqUrl, reqObj);
 
-    const headers: Record<string, string> = {};
-    resp.headers.forEach((value, key) => (headers[key] = value));
+      const headers: Record<string, string> = {};
+      resp.headers.forEach((value, key) => (headers[key] = value));
 
-    let responseData = null;
-    const contentType = resp.headers.get('Content-Type');
+      let responseData = null;
+      const contentType = resp.headers.get('Content-Type');
 
-    if (contentType?.includes('application/json')) {
-      responseData = await resp.json();
-    } else if (contentType?.includes('text/html')) {
-      responseData = await resp.text();
+      if (contentType?.includes('application/json')) {
+        responseData = await resp.json();
+      } else if (contentType?.includes('text/html')) {
+        responseData = await resp.text();
+      }
+
+      const eventReplyObj = {
+        responseData: responseData,
+        responseHeaders: headers,
+        responseCode: resp.status,
+        responseStatusText: resp.statusText,
+        requestUrl: args.reqUrl,
+      };
+
+      event.reply('send-request', eventReplyObj);
+    } catch (err) {
+      // throw 404 if some weird error happens, for now at least :)
+      const eventReplyObj = {
+        responseData: null,
+        responseHeaders: {},
+        responseCode: '404',
+        responseStatusText: 'Not Found',
+        requestUrl: args.reqUrl,
+      };
+
+      event.reply('send-request', eventReplyObj);
     }
-
-    const eventReplyObj = {
-      responseData: responseData,
-      responseHeaders: headers,
-      responseCode: resp.status,
-      responseStatusText: resp.statusText,
-      requestUrl: args.reqUrl
-    };
-
-    event.reply('send-request', eventReplyObj);
   });
 }
