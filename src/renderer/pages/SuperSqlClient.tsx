e22bdcc20cc3135eda5df999f9f55de8c6bd38b7
therealrinku <rinkunited2032@gmail.com>
2024-12-02 19:10:39 +0545

    feat: remove dots

--- src/renderer/pages/SuperSqlClient.tsx
+++ src/renderer/pages/SuperSqlClient.tsx
@@ -195,7 +195,7 @@ export default function SuperSqlClient() {
               <div className="flex flex-col gap-2 items-start">
                 {loadingTables && (
                   <p className="font-bold text-center mt-2">
-                    Loading tables......
+                    Loading tables...
                   </p>
                 )}
                 {!loadingTables &&
